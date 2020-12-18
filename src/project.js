21fdb8bfd087c488bb575fca22f39ff406db3b8a
ABDELLANI-Youcef <youcefabdellani@gmail.com>
2020-12-18 17:21:18 +0100

    Add project class

---
+++ src/project.js
@@ -0,0 +1,28 @@
+class Project {
+  constructor(title){
+    this._title = title;
+    this._tasks = [];
+  }
+  get title(){
+    return this._title;
+  }
+
+  set title(title){
+    this._title = title;
+  }
+
+  get tasks(){
+    return this._tasks;
+  }
+
+  addTask(task){
+    // sort tasks later
+    this._tasks.push(task);
+  }
+
+  removeTask(id){
+    // find index of task by id
+    this._tasks.splice(id,1);//id ===> transformer en index
+  }
+
+}
